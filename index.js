661d22244a48c32b8215ceff35c4e5c402575f0b
ig1711 <813shihab@gmail.com>
2026-01-30 18:09:09 +0600

    init

--- index.js
+++ index.js
@@ -1,92 +1,131 @@
-import process from 'node:process';
-
 import Player from './lib/player.js';
-// import Server from './lib/server.js';
-// import Videos from './lib/videos.js';
+import Server from './lib/server.js';
+import Videos from './lib/videos.js';
 
 const player = new Player();
 
-// const server = new Server();
+const server = new Server();
 
-// const videos = new Videos();
+const videos = new Videos();
 
-// player.on('Connect', async serverAddress => {
-//     if (server.connected) {
-//         player.log(`Already connected to server: ${server.name}`);
-//         return;
-//     }
+player.on('Connect', async ({ address, username }) => {
+    if (server.connected) {
+        player.log(`Already connected to server: ${server.name}`);
+        return;
+    }
 
-//     try {
-//         await server.connect(serverAddress);
-//         player.log(`Connected to server: ${server.name}`);
+    try {
+        await server.connect(address, username);
+        console.log({ username, address });
 
-//         player.log('Syncing playlist...');
+        player.connected();
+        player.log('Connected');
 
-//         try {
-//             const playlist = await server.getPlaylist();
-//             for (let { id, url } of playlist) {
-//                 videos.add(id, url);
-//             }
-//             player.log(`Synced playlist. (Added ${playlist.length} items)`);
-//         } catch (e) {
-//             player.log('Error syncing playlist');
-//             console.error(e);
-//         }
-//     } catch (err) {
-//         player.log('Error connecting to the server');
-//         console.error(err);
-//     }
-// });
+        const serverName = await server.getServerName();
 
-// player.on('Disconnect', async () => {
-//     if (!server.connected) {
-//         player.log('Not connected to any server');
-//         return;
-//     }
-//     const serverName = server.name;
-//     try {
-//         await server.disconnect();
-//         player.log(`Disconnected from server: ${serverName}`);
-//     } catch (err) {
-//         player.log('Error disconnecting from the server');
-//         console.error(err);
-//     }
-// });
+        player.log(`Connected to server: ${serverName}`);
 
-player.on('chat_message', async message => {
-    // await server.broadcastMessage(message).catch(console.error);
-    console.log(message);
+        player.log('Syncing playlist...');
+
+        const playlist = await server.getPlaylist();
+
+        try {
+            for (let { hash, id } of playlist) {
+                videos.add(hash, id);
+            }
+            player.log(`Synced playlist. (Added ${playlist.length} items)`);
+        } catch (e) {
+            player.log('Error syncing playlist');
+            console.error(e);
+        }
+    } catch (err) {
+        player.log('Error connecting to the server');
+        console.error(err);
+    }
 });
 
-// player.on('Paused', async timestamp => {
-//     await server.playerPaused(timestamp).catch(console.error);
-// });
+player.on('Disconnect', async () => {
+    if (!server.connected) {
+        player.log('Not connected to any server');
+        return;
+    }
+    try {
+        server.disconnect();
+        player.log(`Disconnected from server`);
+    } catch (err) {
+        player.log('Error disconnecting from the server');
+        console.error(err);
+    }
+});
+
+player.on('ChatMessage', message => {
+    server.sendMessage(message);
+    // console.log(message);
+});
+
+player.on('Paused', reason => {
+    if (server.connected) server.playerPaused(reason);
+});
 
 // player.on('Resumed', async timestamp => {
 //     await server.playerResumed(timestamp).catch(console.error);
 // });
 
+server.on('NewUser', username => {
+    console.log(`${username} joined`);
+    player.log(`${username} joined`);
+});
 
-// server.on('chat_message', message => {
-//     player.chatMessage(message);
-// });
+server.on('Disconnected', () => {
+    player.pause();
+    player.disconnected();
+});
 
-// server.on('add_video', ({ id, url }) => {
-//     videos.add(id, url);
-// });
+server.on('ChatMessage', message => {
+    player.chatMessage(message);
+});
 
-// server.on('load_video', id => {
-//     const video = videos.get(id);
-//     player.load(video);
-// });
+server.on('Add', ({ hash, id }) => {
+    videos.add(hash, id);
+});
 
-// server.on('play_video', () => {
-//     player.play();
-// });
+server.on('Load', async ({ hash, id }) => {
+    const video = videos.get(hash, id);
+    await video.load();
+});
 
-// server.on('pause_video', () => {
-//     player.pause();
-// });
+server.on('Remove', ({ hash, id }) => {
+    videos.remove(hash, id);
+});
+
+server.on('MpvLoad', async ({ hash, id }) => {
+    const video = videos.get(hash, id);
+    if (!video.loaded) await video.load();
+    const link = await video.url();
+    player.pause();
+    player.load(link);
+});
+
+server.on('Play', () => {
+    player.play();
+});
+
+server.on('Pause', ({ username, reason }) => {
+    console.log(reason);
+    const user_p = (reason === 'User') && 'bcz they wanted';
+    const network_p = (reason === 'Network') && 'bcz of network';
+
+    player.log(`${username} paused the video ${network_p || user_p || ''}`);
+    player.pause();
+});
+
+server.on('Ping', async () => {
+    const videoStatus = await videos.generateStatus();
+
+    const playerStatus = await player.getStatus().catch(() => null);
+
+    server.status({ videoStatus, playerStatus });
+});
 
 // server.on('seek_video', timestamp => {
 //     player.seek(timestamp);
@@ -102,16 +141,32 @@ player.on('chat_message', async message => {
 //     await server.sendTimestamp(timestamp).catch(console.error);
 // });
 
+// let count = 0;
 // setInterval(() => {
 //     if (player.connection) {
-//         player.log('log test');
-//         player.sendMessage({ username: 'something', content: 'some content' });
-//         player.sendCommand({ 'Play': null });
+//         // player.log('log test');
+//         player.sendMessage({ username: `user ${count}`, content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`.slice(Math.floor(Math.random() * 573 / 2), (574 / 2) + Math.floor(Math.random() * 573 / 2)) + count, timestamp: Date.now() });
+//         count += 1;
+//         // player.sendCommand({ 'Play': null });
 //     }
-// }, 3000);
+// }, 5000);
 
-process.on('SIGINT', () => {
-    console.log('exiting bye bye');
-    player.destruct();
-    process.exit();
-});
+// setTimeout(() => {
+//     if (player.connection) player.sendCommand({ 'Play': null });
+// }, 10000);
+
+// process.on('SIGINT', () => {
+//     console.log('exiting bye bye');
+//     player.destruct();
+//     process.exit();
+// });
+
+// const videos = new Videos();
+
+// const testMagnet = 'magnet:?xt=urn:btih:b1d89133a31c8f935f7b2a4949d0e02c2dd0b589&dn=%5BErai-raws%5D%20Sanda%20-%2001%20%5B1080p%20AMZN%20WEBRip%20HEVC%20EAC3%5D%5BMultiSub%5D%5B7EA1279C%5D&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce';
+
+// videos.add('someid', testMagnet);
+
+// const video = videos.get('someid');
+
+// console.log(await video.url());
